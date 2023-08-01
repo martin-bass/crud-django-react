c343d2ef8cfb6f74dc7ba2995b10ad5473132da2
martin-bass <martin_reym@hotmail.com>
2023-08-01 17:55:15 -0300

    setting update3

--- api/settings.py
+++ api/settings.py
@@ -146,7 +146,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # cors autorizacion
 CORS_ALLOWED_ORIGINS = [
-    'https://crud-django-react.vercel.app',
+    'https://crud-django-react.vercel.app/'
     #'https://crud-django-react.onrender.com',
     'http://localhost:5173/'
     ]
