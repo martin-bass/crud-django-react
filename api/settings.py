1d232bc4be3c929ddfcc7fb0edb9ef86162d12eb
martin-bass <martin_reym@hotmail.com>
2023-08-02 17:29:25 -0300

    update12

--- api/settings.py
+++ api/settings.py
@@ -156,9 +156,9 @@ CORS_ALLOWED_ORIGINS = [
     ]
 CORS_ALLOW_ALL_ORIGINS = True
 
-# CORS_ALLOWED_ORIGIN_REGEXES = [
-# r"^https://\w+\.domain\.com$",
-# ]
+CORS_ALLOWED_ORIGIN_REGEXES = [
+r"^https://\w+\.domain\.com$",
+]
 
 
 REST_FRAMEWORK = {
