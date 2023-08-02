cf146f72a71a8445f28470cada41188d7adf689e
martin-bass <martin_reym@hotmail.com>
2023-08-02 17:27:05 -0300

    update settings11

--- api/settings.py
+++ api/settings.py
@@ -154,10 +154,7 @@ CORS_ALLOWED_ORIGINS = [
     'https://crud-django-react.vercel.app',
     'http://localhost:5173'
     ]
-
-CORS_ALLOWED_ORIGIN_REGEXES = [
-r"^https://\w+\.domain\.com$",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 # CORS_ALLOWED_ORIGIN_REGEXES = [
 # r"^https://\w+\.domain\.com$",
