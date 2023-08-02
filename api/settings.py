0604896a62a6babe75b3fca47e416035a0178c4e
Martín Lopez <84106998+martin-bass@users.noreply.github.com>
2023-08-02 12:28:03 -0300

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -150,9 +150,11 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173'
     ]
 
-CORS_ALLOWED_ORIGIN_REGEXES = [
-r"^https://\w+\.domain\.com$",
-]
+#CORS_ALLOWED_ORIGIN_REGEXES = [
+#r"^https://\w+\.domain\.com$",
+#]
+
+CORS_ALLOWED_ORIGIN_REGEXES = [*]
 
 CORS_ALLOW_HEADERS = [
 'accept',
