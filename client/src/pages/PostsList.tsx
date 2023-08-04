175a64b6194af405ee183038d4725fe2e5f8117c
martin-bass <martin_reym@hotmail.com>
2023-08-04 19:47:32 -0300

    add spinner

--- client/src/pages/PostsList.tsx
+++ client/src/pages/PostsList.tsx
@@ -7,7 +7,7 @@ import Masonry from "react-masonry-css";
 import "../styles/Masonry.css";
 
 //Chakra UI
-import { Stack } from "@chakra-ui/react";
+import { Spinner, Stack } from "@chakra-ui/react";
 
 //Components
 import CardItem from "../components/CardItem";
@@ -21,26 +21,34 @@ function PostList() {
   };
 
   const [posts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const LoadPosts = async () => {
       const res = await getAllPosts();
       setPosts(res.data);
     };
+    setLoading(true);
     LoadPosts();
   }, []);
 
   return (
     <Stack w={"full"}>
-      <Masonry
-        breakpointCols={breakpointColumnsObj}
-        className="my-masonry-grid"
-        columnClassName="my-masonry-grid_column"
-      >
-        {posts.map((post) => (
-          <CardItem key={post.id} post={post} />
-        ))}
-      </Masonry>
+      {loading ? (
+        <Masonry
+          breakpointCols={breakpointColumnsObj}
+          className="my-masonry-grid"
+          columnClassName="my-masonry-grid_column"
+        >
+          {posts.map((post) => (
+            <CardItem key={post.id} post={post} />
+          ))}
+        </Masonry>
+      ) : (
+        <Stack w={"full"} minH={"100vh"}>
+          <Spinner size="xl" margin={"auto"} />
+        </Stack>
+      )}
     </Stack>
   );
 }
